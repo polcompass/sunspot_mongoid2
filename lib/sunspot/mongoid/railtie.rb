8d1ce7121f6a07daa145104903d2623ca0a9f97d
Hélio Costa <hlegius@gmail.com>
2013-05-26 21:48:10 -0300

    Fixed default paths

--- lib/sunspot/mongoid/railtie.rb
+++ lib/sunspot/mongoid/railtie.rb
@@ -4,7 +4,7 @@ require 'rails'
 module Sunspot::Mongoid
   class Railtie < Rails::Railtie
     rake_tasks do
-      load File.expand_path("../../../../tasks/sunspot_mongoid.rake", __FILE__)
+      load File.expand_path("../../../../tasks/sunspot_mongoid2.rake", __FILE__)
     end
   end
 end
